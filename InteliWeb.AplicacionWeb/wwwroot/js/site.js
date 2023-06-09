49ec3ae49f23f78a1e1deb75b64a736b491c957d
Fernando Carrillo Garcia <ferduzi007fcg@hotmail.com>
2023-06-09 13:11:10 -0600

    Add files via upload

---
+++ InteliWeb.AplicacionWeb/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//let imgScroll = document.querySelectorAll("#div-p-row");
+
+//function mostrarScroll() {
+//    let scrollTop = document.documentElement.scrollTop;
+//    for (let i = 0; i < imgScroll.length; i++) {
+//        let alturaDiv = imgScroll[i].offsetTop;
+//        if (alturaDiv - 500 < scrollTop) {
+//            imgScroll[i].style.opacity = 1;
+//            imgScroll[i].classList.add("dezlizarDiv");
+//        }
+//    }
+//}
+
+//window.addEventListener("scroll", mostrarScroll);
